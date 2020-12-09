a01984cc52b07beb3711c96bf9192ecf18f6a7fc
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2020-12-09 12:18:36 +0100

    declare global json behaviour

--- grpc-server/src/main/java/com/github/anjeyy/api/dto/model/DocumentDto.java
+++ grpc-server/src/main/java/com/github/anjeyy/api/dto/model/DocumentDto.java
@@ -1,13 +1,11 @@
 package com.github.anjeyy.api.dto.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.UUID;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class DocumentDto {
 
     UUID docid;
---
+++ grpc-server/src/main/java/com/github/anjeyy/infrastructure/config/ObjectMapperConfig.java
@@ -0,0 +1,20 @@
+package com.github.anjeyy.infrastructure.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ObjectMapperConfig implements InitializingBean {
+
+    private final ObjectMapper objectMapper;
+    
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        objectMapper.setSerializationInclusion(Include.NON_NULL);
+    }
+}
