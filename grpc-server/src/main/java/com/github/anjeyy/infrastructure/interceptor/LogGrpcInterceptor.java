95166aa85499b5fe7528c545bb8fc640e51e1863
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2020-12-09 12:22:58 +0100

    remove clutter config

--- grpc-server/src/main/java/com/github/anjeyy/infrastructure/interceptor/LogGrpcInterceptor.java
+++ grpc-server/src/main/java/com/github/anjeyy/infrastructure/interceptor/LogGrpcInterceptor.java
@@ -6,8 +6,10 @@ import io.grpc.ServerCall.Listener;
 import io.grpc.ServerCallHandler;
 import io.grpc.ServerInterceptor;
 import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor;
 
 @Slf4j
+@GrpcGlobalServerInterceptor
 public class LogGrpcInterceptor implements ServerInterceptor {
 
     @Override
