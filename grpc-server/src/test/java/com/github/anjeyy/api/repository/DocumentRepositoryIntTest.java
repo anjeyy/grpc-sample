1fdc7130e592de7d6cba2e6354c09a8c1b3e5e16
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2020-12-21 16:19:54 +0100

    gRPC unit testing added

--- grpc-server/src/test/java/com/github/anjeyy/api/repository/DocumentRepositoryIntTest.java
+++ grpc-server/src/test/java/com/github/anjeyy/api/repository/DocumentRepositoryIntTest.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@Sql(scripts = "classpath:db-test-setup.sql")
 class DocumentRepositoryIntTest {
 
     @Autowired
@@ -21,26 +23,36 @@ class DocumentRepositoryIntTest {
     @Test
     void findAll_success() {
         // prepare
-        Document toBeExpected = createExpectedDocument();
+        List<Document> toBeExpected = createExpectedDocument();
 
         // execute
         List<Document> result = uut.findAll();
 
         // verify
-        result.forEach(doc -> Assertions.assertThat(doc).usingRecursiveComparison().isEqualTo(toBeExpected));
+        Assertions.assertThat(result)
+                  .isNotEmpty()
+                  .hasSize(2)
+                  .usingRecursiveComparison()
+                  .isEqualTo(createExpectedDocument());
     }
 
     // ###################
     // ### H E L P E R ###
     // ###################
 
-    private Document createExpectedDocument() {
-        Document toBeExpected = new Document();
-        toBeExpected.setDocid(UUID.fromString("29e84b17-b32c-49b9-8497-63833144c210"));
-        toBeExpected.setTitle("TEST_TITEL");
-        toBeExpected.setPerson("integration-test");
-        toBeExpected.setFilesize(4096);
+    private List<Document> createExpectedDocument() {
+        Document first = new Document();
+        first.setDocid(UUID.fromString("29e84b17-b32c-49b9-8497-63833144c210"));
+        first.setTitle("TEST_TITEL");
+        first.setPerson("integration-test");
+        first.setFilesize(4096);
 
-        return toBeExpected;
+        Document second = new Document();
+        second.setDocid(UUID.fromString("39e84b17-b32c-49b9-8497-63833144c210"));
+        second.setTitle("TEST_TITEL_TWO");
+        second.setPerson("integration-test");
+        second.setFilesize(2048);
+
+        return List.of(first, second);
     }
 }
---
+++ grpc-server/src/test/java/com/github/anjeyy/api/service/GrpcDocumentServiceTest.java
@@ -0,0 +1,103 @@
+package com.github.anjeyy.api.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.github.anjeyy.proto.document.DocumentResponse;
+import com.github.anjeyy.proto.document.DocumentServiceGrpc;
+import com.google.protobuf.Empty;
+import io.grpc.internal.testing.StreamRecorder;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@DirtiesContext
+@SpringBootTest(properties = {
+    "grpc.server.inProcessName=test",
+    "grpc.server.port=-1",
+    "grpc.client.inProcess.address=in-process:test",
+})
+class GrpcDocumentServiceTest {
+
+    @GrpcClient("inProcess")
+    private DocumentServiceGrpc.DocumentServiceBlockingStub documentServiceBlockingStub;
+
+    @GrpcClient("inProcess")
+    private DocumentServiceGrpc.DocumentServiceStub documentServiceNonBlockingStub;
+
+
+    @Test
+    @DirtiesContext
+    void getAllDocuments_blockingStub() {
+
+        //given
+        Empty testInput = Empty.newBuilder().build();
+
+        // when
+        Iterator<DocumentResponse> actualResult = documentServiceBlockingStub.getAllDocuments(testInput);
+        Spliterator<DocumentResponse> spliterator =
+            Spliterators.spliteratorUnknownSize(actualResult, Spliterator.ORDERED);
+        List<DocumentResponse> actualResultList =
+            StreamSupport.stream(spliterator, false).collect(Collectors.toList());
+
+        // then
+        assertThat(actualResultList).isNotNull()
+                                    .hasSize(2)
+                                    .usingRecursiveComparison()
+                                    .isEqualTo(createExpected());
+
+    }
+
+    @Test
+    @DirtiesContext
+    void getAllDocuments_nonBlockingStub() throws Exception {
+
+        //given
+        Empty testInput = Empty.newBuilder().build();
+
+        // when
+        StreamRecorder<DocumentResponse> actualResponse = StreamRecorder.create();
+        documentServiceNonBlockingStub.getAllDocuments(testInput, actualResponse);
+        boolean responseReceived = actualResponse.awaitCompletion(3, TimeUnit.SECONDS);
+        if (!responseReceived) {
+            Assertions.fail("Timeout - no response received.");
+        }
+
+        // then
+        assertThat(actualResponse.getError()).isNull();
+        assertThat(actualResponse.getValues()).isNotEmpty()
+                                              .hasSize(2)
+                                              .usingRecursiveComparison()
+                                              .isEqualTo(createExpected());
+
+    }
+
+    // ###################
+    // ### H E L P E R ###
+    // ###################
+
+    private List<DocumentResponse> createExpected() {
+        DocumentResponse first = DocumentResponse.newBuilder()
+                                                 .setDocId("29e84b17-b32c-49b9-8497-63833144c210")
+                                                 .setTitle("TEST_TITEL")
+                                                 .setPerson("integration-test")
+                                                 .setFilesize(4096)
+                                                 .build();
+        DocumentResponse second = DocumentResponse.newBuilder()
+                                                  .setDocId("39e84b17-b32c-49b9-8497-63833144c210")
+                                                  .setTitle("TEST_TITEL_TWO")
+                                                  .setPerson("integration-test")
+                                                  .setFilesize(2048)
+                                                  .build();
+
+        return List.of(first, second);
+    }
+}
